e43047d44611b35344186e856c93c04c43c96bb1
Lucas Weaver <lucas@weavercommunication.com>
2019-08-28 16:32:55 +0200

    Changed Image File Names
    Added "-" to file names.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
         English Courses in Rotterdam | Weaver English
 @endsection
 @section('content')
-<div class="container-fluid bg-white">   
-  <div class="row py-5">  
+<div class="container-fluid bg-white">
+  <div class="row py-5">
     <div class="col-lg-4 col-sm-12 my-auto">
         <div class="card" style="width: 26rem; z-index: 2;">
             <div class="card-body">
@@ -13,7 +13,7 @@
             <form method="POST" action="/courses">
                   <div class="form-group">
                       <label for="exampleFormControlSelect1">Courses:</label>
-                      <select class="form-control" id="exampleFormControlSelect1">              
+                      <select class="form-control" id="exampleFormControlSelect1">
                         <option>General English</option>
                         <option>Business English</option>
                         <option>IELTS Prep</option>
@@ -28,7 +28,7 @@
     </div>
     <div class="col-lg-8 col-sm-12">
         <div class="card bg-dark text-white" style="margin-left: -15%; z-index: 1; border: 0px;">
-            <img src="/images/english courses weaver english.jpeg" class="card-img" alt="english courses" style="object-fit: cover; max-height: 24rem;">
+            <img src="/images/english-courses-weaver-english.jpeg" class="card-img" alt="english courses" style="object-fit: cover; max-height: 24rem;">
             <div class="card-img-overlay">
             </div>
         </div>
@@ -39,13 +39,13 @@
     <h2>Premium English courses customized to your level</h2>
     <p class="lead">Our English courses are designed to help you improve quickly and achieve your specific goals.</p>
   </div>
-  <div class="col-lg-9 col-sm-12">  
+  <div class="col-lg-9 col-sm-12">
     <div class="card-deck">
       <div class="card mt-2">
         <img src="/images/english-course-classroom-rotterdam.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h3 class="card-title">Group Courses</h3>
-          <p class="card-text">Group General & Business English courses that provide you with plenty of 
+          <p class="card-text">Group General & Business English courses that provide you with plenty of
               chances to practice and improve your speaking.</p>
           <p class="card-text"><small class="text-muted"></small></p>
         </div>
@@ -54,7 +54,7 @@
         <img src="/images/weaver english private lessons.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h3 class="card-title">Private Lessons</h3>
-          <p class="card-text">Private one-to-one lessons are the best way to imporve the specific English 
+          <p class="card-text">Private one-to-one lessons are the best way to imporve the specific English
               skills you need.</p>
           <p class="card-text"><small class="text-muted"></small></p>
         </div>
@@ -84,7 +84,7 @@
       <h2>Why Weaver English?</h2>
       <p class="lead">Fun English lessons that help you improve quickly.</p>
     </div>
-    <div class="col-lg-9 col-sm-12">  
+    <div class="col-lg-9 col-sm-12">
       <div class="card-deck">
         <div class="card mt-2">
           <img src="/images/flexible course schedules.jpg" class="card-img-top" alt="...">
@@ -121,8 +121,8 @@
             </div>
     </div>
     <!-- Courses Section -->
-    <div class="row justify-content-center home-course-block-one-2">  
-      <div class="card-deck">  
+    <div class="row justify-content-center home-course-block-one-2">
+      <div class="card-deck">
         <!-- Beginning individual course card one -->
              <div class="card" style="width: 24rem; margin-top: 2rem;">
                 <img src="/images/group english course rotterdam.jpg" class="card-img-top" alt="weaver english general english course in rotterdam">
@@ -130,7 +130,7 @@
                         <h4 class="card-title">Advanced General English Course</h4>
                         <p class="card-text">Monthly access to our Advanced English Course.</p>
                       </div>
-                      <div class="card-footer"> 
+                      <div class="card-footer">
                           <a href="#" class="btn btn-primary">LEARN MORE</a>
                        </div>
               </div>
@@ -142,7 +142,7 @@
                           <h4 class="card-title">Intermediate General English Course</h4>
                           <p class="card-text">Monthly access to our Intermediate English Course.</p>
                     </div>
-                    <div class="card-footer">    
+                    <div class="card-footer">
                         <a href="#" class="btn btn-primary">LEARN MORE</a>
                     </div>
             </div>
@@ -156,7 +156,7 @@
                  </div>
                       <div class="card-footer">
                             <a href="#" class="btn btn-primary">LEARN MORE</a>
-                      </div>    
+                      </div>
             </div>
            <!-- End individual course card three -->
           </div>
@@ -164,13 +164,13 @@
       <div class="row justify-content-center home-course-block-two-2">
       <div class="card-deck">
         <!-- Beginning individual course card four -->
-      <div class="card" style="width: 24rem; margin-top: 2rem;">   
+      <div class="card" style="width: 24rem; margin-top: 2rem;">
           <img src="/images/business english course 2.jpeg" class="card-img-top" alt="weaver english business english course in rotterdam">
                 <div class="card-body">
                   <h4 class="card-title">Business English Course</h4>
                   <p class="card-text">Monthly access to our Advanced English Course.</p>
                 </div>
-                <div class="card-footer"> 
+                <div class="card-footer">
                     <a href="#" class="btn btn-primary">LEARN MORE</a>
                  </div>
         </div>
@@ -182,13 +182,13 @@
                     <h4 class="card-title">IELTS Exam Prep Course</h4>
                     <p class="card-text">Monthly access to our Intermediate English Course.</p>
               </div>
-              <div class="card-footer">      
+              <div class="card-footer">
                   <a href="#" class="btn btn-primary">LEARN MORE</a>
               </div>
       </div>
      <!-- End individual course card five -->
       <!-- Beginning individual course card six -->
-    <div class="card" style="width: 24rem; margin-top: 2rem;"> 
+    <div class="card" style="width: 24rem; margin-top: 2rem;">
       <img src="/images/private english lessons 2 zoomed.jpeg" class="card-img-top" alt="weaver english private lessons">
           <div class="card-body">
                 <h4 class="card-title">Private Lessons</h4>
@@ -196,7 +196,7 @@
            </div>
                 <div class="card-footer">
                       <a href="#" class="btn btn-primary">LEARN MORE</a>
-                </div>    
+                </div>
       </div>
      <!-- End individual course card six -->
       </div>
@@ -245,6 +245,6 @@
       </div>
     </div>
   </div>
-  <!-- End Modal --> 
+  <!-- End Modal -->
   </div>
 @endsection
